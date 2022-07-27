ce0d686e00be2cbdfcc2f5dff7da048d84dd8c15
sdirosa <surai@objectiv.io>
2022-07-27 22:59:48 +0200

    LanguageCodes utility basic scraper

---
+++ tracker/plugins/locale-context/src/generated/LanguageCodes.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2022 Objectiv B.V.
+ */
+
+/**
+ * Do not edit this module.
+ * Run its generator utility instead: core/utilities/src/generateLanguageCodes.ts.
+ */
+
+export const LanguageCodes = [
+  ''
+];
