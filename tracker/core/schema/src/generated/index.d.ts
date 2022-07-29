ad4b04a5c97f5770c0c58e3d299b2702e6745fb9
sdirosa <surai@objectiv.io>
2022-07-29 15:49:56 +0200

    Switch schema from .d.ts abstract classes to simple interfaces for improved compatibility

--- tracker/core/schema/src/generated/index.d.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2022 Objectiv B.V.
- */
-
-export * from './abstracts';
-export * from './discriminators';
-export * from './events';
-export * from './global_contexts';
-export * from './location_contexts';
