8461c6c1c6ae95de289d27dfe3b45cf716141eac
thijs <thijs@getobjectiv.com>
2022-08-03 16:12:57 +0200

    cleanup import

--- bach/tests/functional/bach/test_data_and_utils.py
+++ bach/tests/functional/bach/test_data_and_utils.py
@@ -18,7 +18,6 @@ from bach import DataFrame, Series
 from bach.types import get_series_type_from_db_dtype
 from sql_models.constants import DBDialect
 from sql_models.util import is_bigquery, is_postgres
-from tests.conftest import DB_PG_TEST_URL
 from tests.unit.bach.util import get_pandas_df
 
 # Three data tables for testing are defined here that can be used in tests
